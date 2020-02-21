655482137752dea3b75268a0c99f63f24a33b64b
seiki tsunoda <liquidohr@gmail.com>
2020-02-20 22:34:26 -0800

    updated favicon #5

--- gatsby-config.js
+++ gatsby-config.js
@@ -189,7 +189,7 @@ module.exports = {
         background_color: '#FFF',
         theme_color: '#F7A046',
         display: 'standalone',
-        icon: 'static/logo-EigoJS.png'
+        icon: 'static/icon-256x256.png'
       }
     },
     'gatsby-plugin-offline',
