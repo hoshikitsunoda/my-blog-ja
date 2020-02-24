a09ef2a90ae1206fb2fe0e1236619a21149d0afb
seiki tsunoda <liquidohr@gmail.com>
2020-02-24 01:09:12 -0800

    added dark mode #7

--- src/components/Layout/DarkModeSwitch/DarkModeSwitch.js
+++ src/components/Layout/DarkModeSwitch/DarkModeSwitch.js
@@ -1,23 +1,20 @@
 // @flow strict
-import React, { useState } from 'react'
+import React from 'react'
 import Switch from 'react-switch'
 import useDarkMode from 'use-dark-mode'
 import styles from './DarkModeSwitch.module.scss'
 
 const DarkModeSwitch = () => {
   const darkMode = useDarkMode(false)
-  const [isToggled, setToggled] = useState(true)
-  const toggleTrueFalse = () => {
-    setToggled(!isToggled)
-    return isToggled ? darkMode.enable() : darkMode.disable()
-  }
 
   return (
     <div className={styles.darkmodeWrapper}>
       <Switch
         className={styles.darkmodeSwitch}
-        onChange={toggleTrueFalse}
-        checked={!isToggled}
+        onChange={darkMode.toggle}
+        checked={darkMode.value}
+        onColor={'#888888'}
+        offColor={'#140e1b'}
       />
     </div>
   )
