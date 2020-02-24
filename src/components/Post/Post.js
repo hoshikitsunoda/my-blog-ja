b81a3fc7e1f3f94eeacb76f80c5952282da6dfc1
seiki tsunoda <liquidohr@gmail.com>
2020-02-24 00:12:53 -0800

    removed console.log #7

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,32 +1,33 @@
 // @flow strict
-import React from 'react';
-import { Link } from 'gatsby';
-import Author from './Author';
-import Comments from './Comments';
-import Content from './Content';
-import Meta from './Meta';
-import Tags from './Tags';
+import React from 'react'
+import { Link } from 'gatsby'
+import Author from './Author'
+import Comments from './Comments'
+import Content from './Content'
+import Meta from './Meta'
+import Tags from './Tags'
 import ShareButtons from './ShareButtons'
-import styles from './Post.module.scss';
-import type { Node } from '../../types';
+import styles from './Post.module.scss'
+import type { Node } from '../../types'
 
 type Props = {
   post: Node
-};
+}
 
 const Post = ({ post, site }: Props) => {
-  const { html } = post;
-  const { tagSlugs, slug } = post.fields;
-  const { tags, title, date } = post.frontmatter;
-  let array = []
+  const { html } = post
+  const { tagSlugs, slug } = post.fields
+  const { tags, title, date } = post.frontmatter
+  const array = []
   tagSlugs.forEach(item => {
     array.push(item.substring(5, item.length - 1))
   })
-  console.log(slug)
 
   return (
     <div className={styles['post']}>
-      <Link className={styles['post__home-button']} to="/">全ての記事</Link>
+      <Link className={styles['post__home-button']} to="/">
+        全ての記事
+      </Link>
 
       <div className={styles['post__content']}>
         <Content body={html} title={title} />
@@ -34,11 +35,12 @@ const Post = ({ post, site }: Props) => {
 
       <div className={styles['post__footer']}>
         <h3>シェアお願いします！</h3>
-        <ShareButtons 
-          tags={array} 
-          url={site.domain + slug} 
-          twitterHandle={site.twitterHandle} 
-          title={title} />
+        <ShareButtons
+          tags={array}
+          url={site.domain + slug}
+          twitterHandle={site.twitterHandle}
+          title={title}
+        />
         <Meta date={date} />
         {tags && tagSlugs && <Tags tags={tags} tagSlugs={tagSlugs} />}
         <Author />
@@ -48,7 +50,7 @@ const Post = ({ post, site }: Props) => {
         <Comments postSlug={slug} postTitle={post.frontmatter.title} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Post;
+export default Post
